e70595639ec7047d8c579c427d9ba3e1181c1ec8
DomiWok01 <105643573+DomiWok01@users.noreply.github.com>
2022-07-12 20:59:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,21 @@ bot.on("message", async message => {
     }
     
     
+  if(cmd === `${prefix}embedsay`){
+    if(message.member.hasPermission("KICK_MEMBERS")){
+        if(args[0]){
+            let say_embed = new Discord.MessageEmbed()
+            .setDescription(args.join(" "))
+            .setColor("RANDOM")
+            .setTimestamp(message.createdAt)
+            .setFooter(bot.user.username)
 
+            message.channel.send(say_embed);
+        } else {
+        message.reply(`Használat: ${prefix}embedsay <üzenet>`)
+        }
+    } else message.reply("Ehhez nincs jogod! (KICK_MEMBER jogot igényel!")
+}
 
 
     if(cmd === `${prefix}suggest`){
